2f18b00cde2e37853e2704f8df82d3b721a2b10a
Muhammed Rizal N P <risalsalu15@gmail.com>
2025-10-02 09:33:19 +0530

    Update ITaskService.cs

--- JWT-TaskManagement/Services/ITaskService.cs
+++ JWT-TaskManagement/Services/ITaskService.cs
@@ -4,7 +4,7 @@ namespace TaskApiDemo.Services
 {
     public interface ITaskService
     {
-        TaskItem Create(TaskItem task);
+        TaskItem Creat(TaskItem task);
         IEnumerable<TaskItem> GetAll();
         TaskItem? GetById(int id);
         bool Update(int id, TaskItem task);
